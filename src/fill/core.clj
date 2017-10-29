28204c6184fe20e7b9efe68b39c4f46953ec9ab3
Arkadiy Drovosekov <arkdro@gmail.com>
2017-10-29 21:52:58 +0100

    prepare start parameters and run fill

--- src/fill/core.clj
+++ src/fill/core.clj
@@ -1,6 +1,24 @@
 (ns fill.core
   (:gen-class))
 
+(defn get_random_color
+  "Get random color"
+  [color_range]
+  (rand-int color_range))
+
+(defn run
+  "Get params and run fill"
+  [color_range width height]
+  (let [data (create_data color_range width height)
+        plate {:color_range color_range
+               :width width
+               :height height
+               :data data}
+        node {:x 0
+              :y 0}
+        new_color (get_random_color color_range)]
+    (fill.by_points/fill node new_color plate)))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
