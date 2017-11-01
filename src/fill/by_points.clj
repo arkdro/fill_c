a91221f9930c88352b2c6432b6683fe7b4fe1c29
Arkadiy Drovosekov <arkdro@gmail.com>
2017-11-01 18:56:20 +0100

    find_end_of_cut

--- src/fill/by_points.clj
+++ src/fill/by_points.clj
@@ -1,6 +1,15 @@
 (ns fill.by_points
   (:require [fill.plate :as pl]))
 
+(defn find_end_of_cut
+  "Increase the coordinate until the color of the node no longer matches
+  the target color"
+  [node target_color plate]
+  (cond
+    (pl/end_of_line? node plate) node
+    (pl/not_same_colors? node target_color plate) node
+    :default (recur (pl/next_node node) target_color plate)))
+
 (defn process_one_node
   "Extract one node from a queue and do the filling for it"
   [queue target_color new_color plate]
