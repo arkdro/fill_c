4c13f62f3be8ce67cf0c22218122b23bfa46985f
Arkadiy Drovosekov <arkdro@gmail.com>
2017-11-04 23:30:42 +0100

    find_begin_of_cut_test

---
+++ test/fill/by_points_test.clj
@@ -0,0 +1,13 @@
+(ns fill.by_points_test
+  (:require [clojure.test :refer :all]
+            [fill.by_points :refer :all]))
+
+(deftest find_begin_of_cut_test
+  (testing "find begin of cut"
+    (let [node {:x 1, :y 1}
+          target_color 4
+          plate {:data [[1 3 5 1 2] [4 4 4 2 6] [1 2 2 1 1]]}
+          act (find_begin_of_cut node target_color plate)
+          exp {:x 0, :y 1}]
+      (is (= act exp)))))
+
