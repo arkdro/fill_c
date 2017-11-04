903666ae79e0f295272c896645cc88525fea1ae9
Arkadiy Drovosekov <arkdro@gmail.com>
2017-11-04 22:08:42 +0100

    silence stub test

--- test/fill/core_test.clj
+++ test/fill/core_test.clj
@@ -2,6 +2,6 @@
   (:require [clojure.test :refer :all]
             [fill.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest stub-test
+  (testing "stub"
+    (is (= 0 0))))
