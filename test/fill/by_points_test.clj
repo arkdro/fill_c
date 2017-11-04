385769f4ca7b0f8392aec94b43dc2011b41ed4da
Arkadiy Drovosekov <arkdro@gmail.com>
2017-11-04 23:34:16 +0100

    find_end_of_cut_test

--- test/fill/by_points_test.clj
+++ test/fill/by_points_test.clj
@@ -11,3 +11,13 @@
           exp {:x 0, :y 1}]
       (is (= act exp)))))
 
+(deftest find_end_of_cut_test
+  (testing "find end of cut"
+    (let [node {:x 1, :y 1}
+          target_color 4
+          plate {:data [[1 3 5 1 2] [4 4 4 2 6] [1 2 2 1 1]]
+                 :width 5}
+          act (find_end_of_cut node target_color plate)
+          exp {:x 2, :y 1}]
+      (is (= act exp)))))
+
