b83cd61e39ff971bcb76f7772280f5212122ae95
Arkadiy Drovosekov <arkdro@gmail.com>
2017-11-07 23:57:48 +0100

    process_one_node_test

--- test/fill/by_points_test.clj
+++ test/fill/by_points_test.clj
@@ -155,3 +155,25 @@
       (is (= act_seq exp_seq))
       (is (= act_plate exp_plate)))))
 
+(deftest process_one_node_test
+  (testing "process one node"
+    (let [node {:x 2, :y 1}
+          empty_queue (clojure.lang.PersistentQueue/EMPTY)
+          queue (conj empty_queue node)
+          target_color 4
+          new_color 6
+          plate {:data [[4 3 5 1 6] [4 4 4 2 6] [5 3 4 1 1]]
+                 :width 5
+                 :height 3}
+          [new_queue act_plate] (process_one_node queue
+                                                  target_color
+                                                  new_color ; unused?
+                                                  plate)
+          act_seq (seq new_queue)
+          exp_seq [{:x 0, :y 0} {:x 2, :y 2}]
+          exp_plate {:data [[4 3 5 1 6] [4 4 4 2 6] [5 3 4 1 1]]
+                     :width 5
+                     :height 3}]
+      (is (= act_seq exp_seq))
+      (is (= act_plate exp_plate)))))
+
