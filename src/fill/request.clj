a52ddaedf6a3f5644376359bd16ad02e1f42112b
Arkadiy Drovosekov <arkdro@gmail.com>
2017-11-14 00:07:33 +0100

    stub for write_request

--- src/fill/request.clj
+++ src/fill/request.clj
@@ -5,6 +5,12 @@
   []
   )
 
+(defn write_request
+  "Write a request to a file"
+  [dir idx request]
+  (println dir idx request)
+  )
+
 (defn generate_requests
   ""
   [dir amount width height color_range]
