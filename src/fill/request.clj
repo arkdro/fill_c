7ce316e00bf7b03ac9dea51b9d39e087404d704c
Arkadiy Drovosekov <arkdro@gmail.com>
2017-11-14 00:07:58 +0100

    generate_requests

--- src/fill/request.clj
+++ src/fill/request.clj
@@ -13,7 +13,9 @@
   )
 
 (defn generate_requests
-  ""
+  "Create a bunch of request files in the specified directory"
   [dir amount width height color_range]
-  )
+  (dotimes [idx amount]
+    (let [request (generate_one_request width height color_range)]
+      (write_request dir idx request))))
 
