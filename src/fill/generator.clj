de365b4212505cace14b0ded5046ddb3f0d918cd
Arkadiy Drovosekov <arkdro@gmail.com>
2017-11-18 00:12:07 +0100

    fix loop condition

--- src/fill/generator.clj
+++ src/fill/generator.clj
@@ -22,20 +22,22 @@
   [min max]
   (random_in_range min max))
 
-(defn generate-requests
-  "Generate requests"
-  [{:keys [min-width max-width
+(defn generate_one_request
+  "Generate one randomized request"
+  [idx
+   {:keys [min-width max-width
            min-height max-height
            min-color-range max-color-range
-           dir num-request]
+           dir]
     :as opts}]
-  (if (> num-request 0)
-    (let [width (generate_width min-width max-width)
-          height (generate_height min-height max-height)
-          color_range (generate_color_range min-color-range max-color-range)
-          opts2 (update opts :num-request dec)
-          idx (get opts :idx 0)
-          new_opts (assoc opts :idx (inc idx))]
-      (fill.request/generate_request dir idx width height color_range)
-      (recur new_opts))))
+  (let [width (generate_width min-width max-width)
+        height (generate_height min-height max-height)
+        color_range (generate_color_range min-color-range max-color-range)]
+    (fill.request/generate_request dir idx width height color_range)))
+
+(defn generate-requests
+  "Generate randomized requests"
+  [{amount :num-request :as opts}]
+  (dotimes [idx amount]
+    (generate_one_request idx opts)))
 
