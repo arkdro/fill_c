ec1b07cff600f13d20bd377e022e3009105ebd16
Arkadiy Drovosekov <arkdro@gmail.com>
2017-11-25 23:27:57 +0100

    add ccl

---
+++ src/ccl/ccl.clj
@@ -0,0 +1,11 @@
+(ns ccl.ccl
+  "Connected component labeling. Based on 'A linear-time two-scan labeling
+  algorithm' by He, Chao, Suzuki."
+  )
+
+(defn ccl
+  "Do CCL"
+  [width height color data]
+  
+  )
+
