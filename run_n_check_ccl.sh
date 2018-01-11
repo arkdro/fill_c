ed4d1a74ab679877fa6ea636d7b03f7939213ab5
Arkadiy Drovosekov <arkdro@gmail.com>
2018-01-12 00:20:47 +0100

    add run n check script for ccl

---
+++ run_n_check_ccl.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# script to run ccl generator and checker together
+
+do_one_dir(){
+    dir="$1"
+    generator_params="-d $dir -n $number_of_requests \
+		--min-width $min_width --max-width $max_width \
+		--min-height $min_height --max-height $max_height \
+		--probability $probability \
+		--ccl-output-background -1 \
+        -t ccl \
+		--min-color-range $min_color_range --max-color-range $max_color_range"
+    checker_params="--remove --indir $dir"
+    $generator $generator_params
+    $checker $checker_params
+}
+
+export RLOG_LOG_LEVEL=ERROR
+
+generator="java -jar fill.jar"
+checker="./ccl_g"
+number_of_requests=10
+min_width=4
+max_width=100
+min_height=4
+max_height=100
+min_color_range=4
+max_color_range=10
+probability=50
+
+start_dir="${1:-.}"
+base="${start_dir}/req"
+
+batches=10
+n=0
+while ((n<batches))
+do
+    echo "batch: $n, `date`"
+    dir="${base}/$n"
+    mkdir -p "$dir"
+    do_one_dir "$dir"
+    rmdir --ignore-fail-on-non-empty "$dir"
+    if [ -d "$dir" ] ; then
+        echo "some errors in batch: $n, dir: $dir"
+    fi
+    ((n += 1))
+done
+
