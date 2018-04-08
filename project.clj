79a8b2b0117a76bd6b23bef3c270baf16b7033df
Arkadiy Drovosekov <arkdro@gmail.com>
2018-04-08 19:40:33 +0200

    use clojure 1.9.0

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-beta2"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/tools.cli "0.3.5"]
                  [cheshire "5.8.0"]]
   :main ^:skip-aot fill.core
