c4e18978ea7fc8d282d03e54766d15bc8bb9b78a
Arkadiy Drovosekov <arkdro@gmail.com>
2018-04-23 22:25:39 +0200

    merging one color

--- src/table/table.clj
+++ src/table/table.clj
@@ -4,12 +4,24 @@
   [acc ccl_data]
   )
 
+(defn merge_one_color
+  [{:keys [color width height data] :as acc} one_color_ccl_data]
+  (let [coordinates (ccl.ccl/generate_coordinates width height)
+        new_data (reduce #(merge_one_color_over_coordinates %1 %2 color
+                                                            one_color_ccl_data)
+                         data coordinates)]
+    (assoc acc
+           :color (inc color)
+           :data new_data)))
+
 (defn merge_ccl_data
   "Get ccl arrays for different colors and merge them in a one array"
   [width height ccl_data]
   (let [color 0
         acc_data (ccl.ccl/init_2d_array width height)
         acc {:color color
+             :width width
+             :height height
              :data acc_data}
         {res_data :data} (reduce merge_one_color acc ccl_data)]
     res_data))
