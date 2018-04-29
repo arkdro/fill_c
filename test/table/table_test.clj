d3e219013980a67ba2d77b632104d891a3f81623
Arkadiy Drovosekov <arkdro@gmail.com>
2018-04-29 21:39:03 +0200

    add test for merge_ccl_data

--- test/table/table_test.clj
+++ test/table/table_test.clj
@@ -45,3 +45,42 @@
           ]
       (is (= act exp)))))
 
+(deftest merge_ccl_data_test
+  (testing "merge_ccl_data"
+    (let [width 9
+          height 4
+          data [[3 2 2 2 2 2 3 3 3]
+                [3 2 2 0 0 2 2 2 1]
+                [0 0 0 3 0 1 1 1 1]
+                [2 0 0 3 0 3 1 3 0]]
+          ccl_data [[[:no :no :no :no :no :no :no :no :no]
+                     [:no :no :no 1 1 :no :no :no :no]
+                     [1 1 1 :no 1 :no :no :no :no]
+                     [:no 1 1 :no 1 :no :no :no 3]]
+                    [[:no :no :no :no :no :no :no :no :no]
+                     [:no :no :no :no :no :no :no :no 1]
+                     [:no :no :no :no :no 1 1 1 1]
+                     [:no :no :no :no :no :no 1 :no :no]]
+                    [[:no 1 1 1 1 1 :no :no :no]
+                     [:no 1 1 :no :no 1 1 1 :no]
+                     [:no :no :no :no :no :no :no :no :no]
+                     [2 :no :no :no :no :no :no :no :no]]
+                    [[1 :no :no :no :no :no 2 2 2]
+                     [1 :no :no :no :no :no :no :no :no]
+                     [:no :no :no 3 :no :no :no :no :no]
+                     [:no :no :no 3 :no 4 :no 5 :no]]]
+          act (merge_ccl_data width height ccl_data)
+          exp [[{:c 3, :l 1} {:c 2, :l 1} {:c 2, :l 1} {:c 2, :l 1}
+                {:c 2, :l 1} {:c 2, :l 1} {:c 3, :l 2} {:c 3, :l 2}
+                {:c 3, :l 2}]
+               [{:c 3, :l 1} {:c 2, :l 1} {:c 2, :l 1} {:c 0, :l 1}
+                {:c 0, :l 1} {:c 2, :l 1} {:c 2, :l 1} {:c 2, :l 1}
+                {:c 1, :l 1}]
+               [{:c 0, :l 1} {:c 0, :l 1} {:c 0, :l 1} {:c 3, :l 3}
+                {:c 0, :l 1} {:c 1, :l 1} {:c 1, :l 1} {:c 1, :l 1}
+                {:c 1, :l 1}]
+               [{:c 2, :l 2} {:c 0, :l 1} {:c 0, :l 1} {:c 3, :l 3}
+                {:c 0, :l 1} {:c 3, :l 4} {:c 1, :l 1} {:c 3, :l 5}
+                {:c 0, :l 3}]]
+          ]
+      (is (= act exp)))))
