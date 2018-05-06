fe6c14b32f271840eb54a002b6fdda81287a24e7
Arkadiy Drovosekov <arkdro@gmail.com>
2018-05-06 22:11:02 +0200

    move coordinates' functions to a separate module

--- src/ccl/ccl.clj
+++ src/ccl/ccl.clj
@@ -1,7 +1,7 @@
 (ns ccl.ccl
   "Connected component labeling. Based on 'A linear-time two-scan labeling
   algorithm' by He, Chao, Suzuki."
-  )
+  (:require [ccl.coordinates]))
 
 (defn- init_1d_array
   [width]
@@ -38,87 +38,11 @@
 (def background_point?
   (complement foreground_point?))
 
-(defn get_left_coord
-  "Get coordinates of a point on the left"
-  [{:keys [x y]}]
-  (if (= x 0) nil
-      {:x (dec x)
-       :y y}))
-
-(defn get_upper_coord
-  "Get coordinates of a point on the up"
-  [{:keys [x y]}]
-  (if (= y 0) nil
-      {:x x
-       :y (dec y)}))
-
-(defn get_upper_left_coord
-  "Get coordinates of a point on the upper-left"
-  [{:keys [x y]}]
-  (cond (= x 0) nil
-        (= y 0) nil
-        :default {:x (dec x)
-                  :y (dec y)}))
-
-(defn get_upper_right_coord
-  "Get coordinates of a point on the upper-right"
-  [{:keys [x y]} width]
-  (cond (>= x (- width 1)) nil
-        (= y 0) nil
-        :default {:x (inc x)
-                  :y (dec y)}))
-
-(defn get_mask_coordinates_8
-  "Get coordinates of mask points for the specified coordinates.
-  It uses 8-connectivity."
-  [coord width]
-  (let [left (get_left_coord coord)
-        upper_left (get_upper_left_coord coord)
-        upper (get_upper_coord coord)
-        upper_right (get_upper_right_coord coord width)
-        coordinates [upper_left upper upper_right left]]
-    (filter some? coordinates)))
-
-(defn get_mask_coordinates_6_even
-  "Get coordinates of mask points for the specified coordinates,
-  when the y-coordinate is even.
-  It uses 6-connectivity."
-  [coord width]
-  (let [left (get_left_coord coord)
-        upper (get_upper_coord coord)
-        upper_right (get_upper_right_coord coord width)
-        coordinates [upper upper_right left]]
-    (filter some? coordinates)))
-
-(defn get_mask_coordinates_6_odd
-  "Get coordinates of mask points for the specified coordinates,
-  when the y-coordinate is odd.
-  It uses 6-connectivity."
-  [coord width]
-  (let [left (get_left_coord coord)
-        upper_left (get_upper_left_coord coord)
-        upper (get_upper_coord coord)
-        coordinates [upper_left upper left]]
-    (filter some? coordinates)))
-
-(defn get_mask_coordinates_6
-  [{:keys [y] :as coord}
-   width]
-  (if (even? y)
-    (get_mask_coordinates_6_even coord width)
-    (get_mask_coordinates_6_odd coord width)))
-
-(defn get_mask_coordinates
-  [coord width connectivity]
-  (case connectivity
-    4 (ex-info "Connectivity 4 is not implemented" {})
-    6 (get_mask_coordinates_6 coord width)
-    8 (get_mask_coordinates_8 coord width)))
-
 (defn get_mask_colors
   "Get colors for the mask at the specified coordinates"
   [coord width connectivity data]
-  (let [coordinates (get_mask_coordinates coord width connectivity)]
+  (let [coordinates (ccl.coordinates/get_mask_coordinates coord width
+                                                          connectivity)]
     (map #(get_color % data) coordinates)))
 
 (defn background_mask?
---
+++ src/ccl/coordinates.clj
@@ -0,0 +1,79 @@
+(ns ccl.coordinates)
+
+(defn get_left_coord
+  "Get coordinates of a point on the left"
+  [{:keys [x y]}]
+  (if (= x 0) nil
+      {:x (dec x)
+       :y y}))
+
+(defn get_upper_coord
+  "Get coordinates of a point on the up"
+  [{:keys [x y]}]
+  (if (= y 0) nil
+      {:x x
+       :y (dec y)}))
+
+(defn get_upper_left_coord
+  "Get coordinates of a point on the upper-left"
+  [{:keys [x y]}]
+  (cond (= x 0) nil
+        (= y 0) nil
+        :default {:x (dec x)
+                  :y (dec y)}))
+
+(defn get_upper_right_coord
+  "Get coordinates of a point on the upper-right"
+  [{:keys [x y]} width]
+  (cond (>= x (- width 1)) nil
+        (= y 0) nil
+        :default {:x (inc x)
+                  :y (dec y)}))
+
+(defn get_mask_coordinates_8
+  "Get coordinates of mask points for the specified coordinates.
+  It uses 8-connectivity."
+  [coord width]
+  (let [left (get_left_coord coord)
+        upper_left (get_upper_left_coord coord)
+        upper (get_upper_coord coord)
+        upper_right (get_upper_right_coord coord width)
+        coordinates [upper_left upper upper_right left]]
+    (filter some? coordinates)))
+
+(defn get_mask_coordinates_6_even
+  "Get coordinates of mask points for the specified coordinates,
+  when the y-coordinate is even.
+  It uses 6-connectivity."
+  [coord width]
+  (let [left (get_left_coord coord)
+        upper (get_upper_coord coord)
+        upper_right (get_upper_right_coord coord width)
+        coordinates [upper upper_right left]]
+    (filter some? coordinates)))
+
+(defn get_mask_coordinates_6_odd
+  "Get coordinates of mask points for the specified coordinates,
+  when the y-coordinate is odd.
+  It uses 6-connectivity."
+  [coord width]
+  (let [left (get_left_coord coord)
+        upper_left (get_upper_left_coord coord)
+        upper (get_upper_coord coord)
+        coordinates [upper_left upper left]]
+    (filter some? coordinates)))
+
+(defn get_mask_coordinates_6
+  [{:keys [y] :as coord}
+   width]
+  (if (even? y)
+    (get_mask_coordinates_6_even coord width)
+    (get_mask_coordinates_6_odd coord width)))
+
+(defn get_mask_coordinates
+  [coord width connectivity]
+  (case connectivity
+    4 (ex-info "Connectivity 4 is not implemented" {})
+    6 (get_mask_coordinates_6 coord width)
+    8 (get_mask_coordinates_8 coord width)))
+
