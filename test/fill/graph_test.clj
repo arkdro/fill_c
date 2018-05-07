ad5a149c0d2408edc72ed9f143649d09eb8c646e
Arkadiy Drovosekov <arkdro@gmail.com>
2018-05-07 22:55:15 +0200

    add add_different_ids_to_node test

--- test/fill/graph_test.clj
+++ test/fill/graph_test.clj
@@ -13,3 +13,15 @@
                         {:x 0, :y 1}}}
           act (add_same_id_cells_to_node node cells_to_add)]
       (is (= act exp)))))
+
+(deftest add_different_ids_to_node_test
+  (testing "Add cells with diffrent node ids as neigbours"
+    (let [node {:id 1
+                :neigbours #{1}}
+          cells_to_add [{:x 0, :y 0, :id 1}
+                        {:x 0, :y 1, :id 2}]
+          exp {:id 1
+               :neigbours #{1 2}}
+          act (add_different_ids_to_node node cells_to_add)]
+      (is (= act exp)))))
+
