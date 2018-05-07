3d5a567a88e9f2e4954a7e6e28becf97d5177a67
Arkadiy Drovosekov <arkdro@gmail.com>
2018-05-07 22:55:35 +0200

    add_neigbours_to_node test

--- test/fill/graph_test.clj
+++ test/fill/graph_test.clj
@@ -25,3 +25,21 @@
           act (add_different_ids_to_node node cells_to_add)]
       (is (= act exp)))))
 
+(deftest add_neigbours_to_node_test
+  (testing "Add neigbours to a node"
+    (let [node {:id 1
+                :cells #{{:x 2, :y 1}}
+                :neigbours #{5}}
+          same_id [{:x 0, :y 0, :id 1}
+                   {:x 1, :y 1, :id 1}]
+          different_ids [{:x 3, :y 0, :id 2}
+                         {:x 3, :y 1, :id 3}
+                         {:x 3, :y 1, :id 2}]
+          exp {:id 1
+               :cells #{{:x 2, :y 1}
+                        {:x 0, :y 0}
+                        {:x 1, :y 1}}
+               :neigbours #{2 3 5}}
+          act (add_neigbours_to_node node same_id different_ids)]
+      (is (= act exp)))))
+
