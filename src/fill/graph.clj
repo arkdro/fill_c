3c4c2660f2edb24e57349ef9212c792ed888ba89
Arkadiy Drovosekov <arkdro@gmail.com>
2018-05-08 00:28:41 +0200

    fix add_one_neigbour_to_graph

--- src/fill/graph.clj
+++ src/fill/graph.clj
@@ -32,7 +32,9 @@
   "Add the current id to the neigbours of a node and then add this updated node
   to the accumulator"
   [acc {:keys [id]} current_id]
-  (let [{:keys [neigbours] :as node} (get acc id {:neigbours #{} :cells #{}})
+  (let [{:keys [neigbours] :as node} (get acc id {:id id
+                                                  :neigbours #{}
+                                                  :cells #{}})
         new_neigbours (conj neigbours current_id)
         new_node (assoc node :neigbours new_neigbours)]
     (assoc acc id new_node)))
